315a8edb7b11873aaad2e8b4f78d7f89557ccbec
Muhammad Faizan <muhammad.faizan01@confiz.local>
2021-09-15 04:39:00 +0500

    Feature added

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import { connect } from 'react-redux'
 import getVisibleExpenses from '../selectors/expenses'
 import getTotalSum from '../selectors/expense-total'
